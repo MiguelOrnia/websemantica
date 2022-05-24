474ac3878a3cdb29dc518e7be178f62ffe9eb590
andresfdezglez <Andresfpano@gmail.com>
2022-05-24 09:27:34 +0200

    Calculo de duración y movementspeed

--- Ejercicio2/ejercicio2.py
+++ Ejercicio2/ejercicio2.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import numpy as np
 from wikibase_api import Wikibase
+from datetime import datetime
 import json
 
 url = "https://www.ncdc.noaa.gov/stormevents/eventdetails.jsp?id=943554"
@@ -29,10 +30,47 @@ wb = Wikibase(api_url='http://156.35.98.119/w/api.php',
               login_credentials=credentials,
               is_bot=True)
 
+new_date = datetime(2019, 2, 28, 10, 15, 00, 00000)
+
+instanceOf = info[0]
+scale = info[1]
+length = info[2]
+width = info[3]
+country = "United States"
+state = info[4]
+county = info[5]
+source = info[7]
+añoBegin = info[9].split('-')[0]
+mesBegin = info[9].split('-')[1]
+añoEnd = info[12].split('-')[0]
+mesEnd = info[12].split('-')[1]
+endDate = info[12]
+beginDate = info[9]
+lat = info[11].split('/')[0]
+lon = info[11].split('/')[1]
+duration = ""
+deaths = info[15].split('/')[0]
+injuries = info[16].split('/')[0]
+propertyDamage = info[18] + " United States dollar"
+cropDamage = info[19] + " United States dollar"
+
+beginDateObject = datetime(int(añoBegin), int(mesBegin), int(beginDate.split(" ")[0].split("-")[2]), int(beginDate.split(" ")[1].split(":")[0]), int(beginDate.split(" ")[1].split(":")[0]), 00, 00000)
+
+endDateObject = datetime(int(añoEnd), int(mesEnd), int(endDate.split(" ")[0].split("-")[2]),int(endDate.split(" ")[1].split(":")[0]), int(endDate.split(" ")[1].split(":")[1]), 00, 00000)
+
+duration = (endDateObject - beginDateObject).total_seconds()
+
+duration_hours = duration / 3600
+
+movementSpeed = float(length.split(" ")[0])/duration_hours
+
 entity = wb.entity.add("item")
-año = info[9].split('-')[0]
-content = {"labels": {"en": {"language": "en", "value": info[5] + " Tornado " + año}}}
+
+
+content = {"labels": {"en": {"language": "en", "value": info[5] + " Tornado " + añoBegin}}}
 updated = wb.entity.update(entity["entity"]["id"], content=content)
+##wb.claim.add(entity["entity"]["id"], 'P9', length, snak_type='value')
+
 listItems = list()
 pos = 0
 for i in info:
