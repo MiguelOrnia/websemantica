9cbf8a1d740ef7927caa264db67247ab27f0bd7b
andresfdezglez <Andresfpano@gmail.com>
2022-05-25 12:39:38 +0200

    Se añaden propiedades a la entidad

--- Ejercicio2/ejercicio2.py
+++ Ejercicio2/ejercicio2.py
@@ -31,19 +31,33 @@ def createWikiBase():
     }
 
     wikibase = Wikibase(api_url='http://156.35.98.119/w/api.php',
-                  oauth_credentials=None,
-                  login_credentials=credentials,
-                  is_bot=True)
+                        oauth_credentials=None,
+                        login_credentials=credentials,
+                        is_bot=True)
 
     return wikibase
+
+
+wb = createWikiBase()
+
+
+def findEntity(label):
+    r = wb.entity.search(label, 'es')['search']
+    if len(r) > 0:
+        return r[0].get('id')
+    else:
+        newEntity = wb.entity.add("item")
+        entityContent = {"labels": {"en": {"language": "en", "value": label}}}
+        wb.entity.update(newEntity["entity"]["id"], content=entityContent)
+        return newEntity["entity"]["id"]
 
 
 info = obtenerInfo(tornado)
 
 instanceOf = info[0]
 scale = info[1]
-length = info[2]
-width = info[3]
+length = float(info[2].split(" ")[0])
+width = float(info[3].split(" ")[0])
 country = "United States"
 state = info[4]
 county = info[5]
@@ -53,14 +67,15 @@ mesBegin = info[9].split('-')[1]
 añoEnd = info[12].split('-')[0]
 mesEnd = info[12].split('-')[1]
 endDate = info[12]
-beginDate = info[9]
+beginDate = info[9 ]
 lat = info[11].split('/')[0]
 lon = info[11].split('/')[1]
 duration = ""
 deaths = info[15].split('/')[0]
 injuries = info[16].split('/')[0]
-propertyDamage = info[18] + " United States dollar"
-cropDamage = info[19] + " United States dollar"
+propertyDamage = float(info[17].split("M")[0])
+print(propertyDamage)
+cropDamage = float(info[18].split("K")[0])
 
 beginDateObject = datetime(int(añoBegin), int(mesBegin), int(beginDate.split(" ")[0].split("-")[2]),
                            int(beginDate.split(" ")[1].split(":")[0]), int(beginDate.split(" ")[1].split(":")[1]), 00,
@@ -73,27 +88,62 @@ duration = (endDateObject - beginDateObject).total_seconds()
 
 duration_hours = duration / 3600
 
-movementSpeed = float(length.split(" ")[0]) / duration_hours
+movementSpeed = length / duration_hours
 
-wb = createWikiBase()
+idState = findEntity(state)
+idCounty = findEntity(county)
+idState.split('Q')[1]
 
 entity = wb.entity.add("item")
 
-content = {"labels": {"en": {"language": "en", "value": info[5] + " Tornado " + añoBegin}}}
+content = {"labels": {"en": {"language": "en", "value": info[5] + " Tornado " + añoBegin}},
+           "descriptions": {'en': {'language': 'en', 'value': 'weather event'}},
+           "claims": {'P3': [{'mainsnak': {'snaktype': 'value', 'property': 'P3',
+                                           'datavalue': {'value': {'entity-type': 'item', 'numeric-id': 5, 'id': 'Q5'},
+                                                         'type': 'wikibase-entityid'}, 'datatype': 'wikibase-item'},
+                              'type': 'statement', 'rank': 'normal'}],
+                      'P18': [{'mainsnak': {'snaktype': 'value', 'property': 'P18',
+                                            'datavalue': {'value': {'entity-type': 'item', 'numeric-id': 7, 'id': 'Q7'},
+                                                          'type': 'wikibase-entityid'}, 'datatype': 'wikibase-item'},
+                               'type': 'statement', 'rank': 'normal'}],
+                      'P9': [{'mainsnak': {'snaktype': 'value', 'property': 'P9', 'datavalue': {
+                          'value': {'amount': length, 'unit': 'http://156.35.98.119/entity/Q8'}, 'type': 'quantity'},
+                                           'datatype': 'quantity'}, 'type': 'statement', 'rank': 'normal'}],
+                      'P10': [{'mainsnak': {'snaktype': 'value', 'property': 'P10', 'datavalue': {
+                          'value': {'amount': width, 'unit': 'http://156.35.98.119/entity/Q9'}, 'type': 'quantity'},
+                                            'datatype': 'quantity'}, 'type': 'statement', 'rank': 'normal'}],
+                      'P5': [{'mainsnak': {'snaktype': 'value', 'property': 'P5', 'datavalue': {
+                          'value': {'entity-type': 'item', 'numeric-id': 10, 'id': 'Q10'}, 'type': 'wikibase-entityid'},
+                                           'datatype': 'wikibase-item'}, 'type': 'statement', 'rank': 'normal'}],
+                      'P6': [{'mainsnak': {'snaktype': 'value', 'property': 'P6', 'datavalue': {
+                          'value': {'entity-type': 'item', 'numeric-id': idState.split('Q')[1], 'id': idState},
+                          'type': 'wikibase-entityid'},
+                                           'datatype': 'wikibase-item'}, 'type': 'statement', 'rank': 'normal'}],
+                      'P8': [{'mainsnak': {'snaktype': 'value', 'property': 'P8', 'datavalue': {
+                          'value': {'entity-type': 'item', 'numeric-id': idCounty.split('Q')[1], 'id': idCounty},
+                          'type': 'wikibase-entityid'},
+                                           'datatype': 'wikibase-item'}, 'type': 'statement', 'rank': 'normal'}],
+                      'P16': [{'mainsnak': {'snaktype': 'value', 'property': 'P16',
+                                            'datavalue': {'value': source, 'type': 'string'},
+                                            'datatype': 'string'}, 'type': 'statement', 'rank': 'normal'}],
+                      'P15': [{'mainsnak': {'snaktype': 'value', 'property': 'P15', 'datavalue': {
+                          'value': {'amount': duration, 'unit': 'http://156.35.98.119/entity/Q13'}, 'type': 'quantity'},
+                                            'datatype': 'quantity'}, 'type': 'statement', 'rank': 'normal'}],
+                      'P21': [{'mainsnak': {'snaktype': 'value', 'property': 'P21',
+                                            'datavalue': {'value': {'amount': injuries, 'unit': '1'}, 'type': 'quantity'},
+                                            'datatype': 'quantity'}, 'type': 'statement', 'rank': 'normal'}],
+                      'P22': [{'mainsnak': {'snaktype': 'value', 'property': 'P22',
+                                            'datavalue': {'value': {'amount': deaths, 'unit': '1'}, 'type': 'quantity'},
+                                            'datatype': 'quantity'}, 'type': 'statement', 'rank': 'normal'}],
+                      'P23': [{'mainsnak': {'snaktype': 'value', 'property': 'P23', 'datavalue': {
+                          'value': {'amount': propertyDamage, 'unit': 'http://156.35.98.119/entity/Q14'}, 'type': 'quantity'},
+                                            'datatype': 'quantity'}, 'type': 'statement', 'rank': 'normal'}],
+                      'P24': [{'mainsnak': {'snaktype': 'value', 'property': 'P24', 'datavalue': {
+                          'value': {'amount': cropDamage, 'unit': 'http://156.35.98.119/entity/Q14'}, 'type': 'quantity'},
+                                            'datatype': 'quantity'}, 'type': 'statement', 'rank': 'normal'}],
+
+                      }}
+
 updated = wb.entity.update(entity["entity"]["id"], content=content)
-wb.claim.add(entity["entity"]["id"], "P9", length, snak_type='value')
 
-
-def obtenerItemsExistentes():
-    listItems = list()
-    pos = 0
-    for i in info:
-        print(i)
-        r = wb.entity.search(i, 'es')['search']
-        pos = pos + 1
-        if len(r) > 0:
-            listItems.append(r[0].get('id'))
-        return listItems
-
-
-print(obtenerItemsExistentes())
+print(entity["entity"]["id"])
